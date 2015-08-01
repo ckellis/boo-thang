7c0a8cfee120904c0181c1df34eab8af446e8477
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-08-01 16:37:53 -0700

    use cwd to get around Heroku issue

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+process.env.PWD = process.cwd()
+
 app.use(express.compress());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.env.PWD + '/public'));
 
 app.listen(process.env.PORT || 80);
 
